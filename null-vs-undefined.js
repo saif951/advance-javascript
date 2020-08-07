94a178af4ba87802ec65499f6ad308db1c62f279
saif951 <saif951@gmail.com>
2020-08-08 04:52:06 +0600

    null vs undefined is clear for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+// let pakhi;
+// function add(num1, num2){
+//     console.log(num1 + num2);
+        // return
+// }
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+
+// const pramik = {name:"smart dude",phone: 4567743};
+// console.log(pramik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [3, 4, 6]
+console.log(ages[22]);
