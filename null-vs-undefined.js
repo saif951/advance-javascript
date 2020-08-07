22879d06a9fb07439d0d5e72b03e5b650c4d429d
saif951 <saif951@gmail.com>
2020-08-08 04:53:50 +0600

    after save

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,15 +1,15 @@
-// let pakhi;
-// function add(num1, num2){
-//     console.log(num1 + num2);
-        // return
-// }
+let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2);
+        return
+}
 
-// function add(num1, num2){
-//     console.log(num1, num2);
-// }
+function add(num1, num2){
+    console.log(num1, num2);
+}
 
-// const pramik = {name:"smart dude",phone: 4567743};
-// console.log(pramik.gf);
+const pramik = {name:"smart dude",phone: 4567743};
+console.log(pramik.gf);
 
 let fun = undefined;
 console.log(fun);
