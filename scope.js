2e2cffc443ffcb28d68c84cc69ef656689e2de59
saif951 <saif951@gmail.com>
2020-08-09 05:21:23 +0600

    complete

---
+++ scope.js
@@ -0,0 +1,11 @@
+let bonus = 30;
+
+function sum(first, second){
+    let result = first + second;
+    console.log(bonus);
+    return result;
+}
+
+const output = sum(3, 7);
+console.log(bonus);
+console.log(output);
